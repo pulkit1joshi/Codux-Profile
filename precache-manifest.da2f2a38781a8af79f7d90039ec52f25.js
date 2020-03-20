2f557eb4380a9258bf535575390a75450ae74810
PulkitJoshi <email.pulkitj@gmail.com>
2020-03-21 00:49:41 +0530

    Updates

---
+++ precache-manifest.da2f2a38781a8af79f7d90039ec52f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02957d2cf7d686a5dabfecdac5c5361",
+    "url": "/Codux-Profile/index.html"
+  },
+  {
+    "revision": "cc4a5c9f8ea8b2a582f7",
+    "url": "/Codux-Profile/static/css/main.c9b7e7ef.chunk.css"
+  },
+  {
+    "revision": "12c7f9a5d4bd5bb7b4bc",
+    "url": "/Codux-Profile/static/js/2.b1ec76ab.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Codux-Profile/static/js/2.b1ec76ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4a5c9f8ea8b2a582f7",
+    "url": "/Codux-Profile/static/js/main.509e7534.chunk.js"
+  },
+  {
+    "revision": "4d1339d8e498d26d82f6",
+    "url": "/Codux-Profile/static/js/runtime-main.ded9a149.js"
+  }
+]);

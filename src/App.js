fc51b578df88a2144d29a037d2cdcfce485d448b
PulkitJoshi <email.pulkitj@gmail.com>
2020-03-21 00:42:48 +0530

    Basic Setup is Done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 
-import Posts from './Components/Posts';
-import PostForm from './Components/Postform';
+import Main from './Components/Main';
 
-import store from './store';
+import store from './store'
+
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-    Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    </a>
-    </header>
-    <PostForm />
     <hr/>
-    <Posts/>
-
+    <Main/>
     </div>
     </Provider>
     );
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,2 @@
 // Defines constant
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const NEW_POST = 'NEW_POST';
+export const FETCH_USER = 'FETCH_USER';

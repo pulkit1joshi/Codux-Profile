9830a034e4da38b1240d50c08f68dd5310ecaff6
PulkitJoshi <email.pulkitj@gmail.com>
2020-03-27 14:52:47 +0530

    Added Submissions info

---
+++ src/Components/Users/List.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class List extends Component {
+    render() {
+        return (
+            <div style={{ paddingTop: "3rem" }}>
+                <div class="card" >
+                    <div class="card-header bg-info">
+                        {this.props.heading}
+                    </div>
+                    <ul class="list-group list-group-flush" style={{ fontSize: "13px" }}>
+
+                        {this.props.data.map((datapoint, index) => (
+                            <li class="list-group-item d-flex justify-content-between"> {datapoint.name}
+                                <span class="card" style={{ padding: "1px" }}> {datapoint.num} </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
